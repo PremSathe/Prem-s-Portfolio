01bd8a46e486a2e54efd1265dd419b35febf7528
PremSathe <premsathe78@gmail.com>
2025-02-18 10:08:40 +0530

    Updated portfolio

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,6 @@
-import hero from './hero.jpg'
-import GeekRider from './GeekRider.png'
-import edureka from './edureka.png'
+import hero from "./hero.jpg";
+import GeekRider from "./GeekRider.png";
+import edureka from "./edureka.png";
+import mahindra from "./mahindra.png";
 
-export {
-    hero,
-    GeekRider,
-    edureka, 
-  
-}
+export { hero, GeekRider, edureka, mahindra };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <header className='header'>
       <NavLink to='/'>
-      <div className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-      <p className="blue-gradient_text">PREM</p>
-      </div>
+        <div className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+          <p className="blue-gradient_text">HOME</p>
+        </div>
 
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
           About
         </NavLink>
         <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
